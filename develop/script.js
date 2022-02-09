d6f6f894e062c78e1c5c3e2acc9b100aea43f30a
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-02-09 13:25:34 -0700

    added pseudocode

---
+++ develop/script.js
@@ -0,0 +1,9 @@
+// when start button is clicked, start game and timer countdown
+// display question with multiple choices from array
+// when incorrect answer is chosen, deduct 10 seconds from timer
+// display new question and repeat
+// when questions run out, display score - remaining time - and initials
+// set score and initials in localStorage
+// When View High Scores is clicked, get scores and initials from localStorage
+// Organize by high score
+
